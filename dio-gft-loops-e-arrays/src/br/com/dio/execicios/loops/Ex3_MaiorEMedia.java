dd4d969c20619e8297ee5c14b1aaa221f8ce8cf8
PamHDiz <pamhdiz@gmail.com>
2022-01-27 17:01:18 -0300

    Exercício 3

---
+++ dio-gft-loops-e-arrays/src/br/com/dio/execicios/loops/Ex3_MaiorEMedia.java
@@ -0,0 +1,39 @@
+package br.com.dio.execicios.loops;
+
+import java.util.Scanner;
+
+/**
+ * Faça um programa que leia 5 números 
+ * e informe o maior número e a média desses números.
+ */
+
+public class Ex3_MaiorEMedia {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		
+		int numero;
+		int contador = 0;
+		int maior = 0;
+		int soma = 0;
+		
+		do {
+			
+			System.out.println("Número: ");
+			numero = scan.nextInt();
+			
+			if(numero > maior) {
+				maior = numero;
+			}
+			
+			contador++;
+			soma = soma + numero;
+		} while(contador < 5); 
+			
+		
+		System.out.println("O maior número é: " + maior);
+		System.out.println("A média é: " + (soma/contador));
+	}
+
+}
