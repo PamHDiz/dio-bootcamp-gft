a037612ea5e5d675f8eba3df55e13d7dc07e2508
PamHDiz <pamhdiz@gmail.com>
2022-01-28 00:24:15 -0300

    Exercício 2

---
+++ dio-gft-loops-e-arrays/src/br/com/dio/execicios/arrays/Ex2_Consoantes.java
@@ -0,0 +1,34 @@
+package br.com.dio.execicios.arrays;
+
+import java.util.Scanner;
+
+/**
+ * Faça um programa que leia um vetor de 6 caracteres e diga quantas
+ * consoates foram lidas. Imprima as consoantes
+ */
+public class Ex2_Consoantes {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		
+		String[] caracteres = new String[6];
+		
+		
+		for (int i = 0; i < caracteres.length; i++) {
+			System.out.println("Digite a letra " + (i+1) + ": ");
+			caracteres[i] = scan.next();
+		}
+				
+		for(int i = 0; i < caracteres.length; i++) {
+			if(!(caracteres[i].equalsIgnoreCase("a") | 
+					caracteres[i].equalsIgnoreCase("e") | 
+					caracteres[i].equalsIgnoreCase("i") |
+					caracteres[i].equalsIgnoreCase("o") | 
+					caracteres[i].equalsIgnoreCase("u"))) {
+				System.out.println("Contoante: " + caracteres[i]);
+		}
+	}
+
+}
+}
